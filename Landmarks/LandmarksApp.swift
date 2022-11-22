5044cffd55881c8d7acee757b21bfe63e96cd7da
Vaibhava <vaibhava.potturu@gmail.com>
2022-11-22 00:16:16 -0500

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Vaibhava Potturu on 11/22/22.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
