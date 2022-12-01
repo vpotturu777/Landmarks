8eb78ede967d3f8603e21e6394aa5ef9c57f5975
Vaibhava <vaibhava.potturu@gmail.com>
2022-12-01 15:49:19 -0500

    user input start

---
+++ Landmarks/Views/LandmarkRow.swift
@@ -0,0 +1,33 @@
+//
+//  LandmarkRow.swift
+//  Landmarks
+//
+//  Created by Vaibhava Potturu on 11/22/22.
+//
+
+import SwiftUI
+
+struct LandmarkRow: View {
+    var landmark: Landmark
+    
+    var body: some View {
+        HStack {
+            landmark.image.resizable().frame(width: 50, height: 50)
+            Text(landmark.name)
+            
+            Spacer()
+            
+            if landmark.isFavorite {
+                Image(systemName: "star.fill")
+                    .foregroundColor(.yellow)
+            }
+        }
+    }
+}
+
+struct LandmarkRow_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkRow(landmark: landmarks[0])
+            .previewLayout(.fixed(width: 300, height: 70))
+    }
+}
